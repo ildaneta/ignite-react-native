0902896ab9a6657fb28614a372c9e0031785c602
ildaneta <iildaemanoely@gmail.com>
2021-08-09 23:18:52 -0300

    feat: Creating the home page

---
+++ myskillsapp/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home page</Text>
+    </View>
+  );
+};
+
+export default Home;
