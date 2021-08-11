86f05f33124f6c816022d40afa4092eecd7cba20
ildaneta <iildaemanoely@gmail.com>
2021-08-11 19:44:40 -0300

    feat: Creating the home page and its styles

--- myskillsapp/src/pages/Home/index.js
+++ myskillsapp/src/pages/Home/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, FlatList} from 'react-native';
 
-// import { Container } from './styles';
+import Button from '../../components/Button/index';
+import SkillCard from '../../components/SkillCard/index';
+import {styles} from './styles';
 
 const Home = () => {
+  const [newSkill, setNewSkill] = useState('');
+  const [allSkills, setAllSkills] = useState([]);
+
+  function handleAddNewSkill() {
+    setAllSkills(oldState => [...oldState, newSkill]);
+    setNewSkill('');
+  }
+
+  function handleInputChange(skill) {
+    setNewSkill(skill);
+  }
+
   return (
-    <View>
-      <Text>Home page</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Ilda!</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#666"
+        onChangeText={text => handleInputChange(text)}
+        value={newSkill}
+      />
+
+      <Button onPress={handleAddNewSkill} />
+
+      <Text style={[styles.title, {marginVertical: 40}]}>My Skills: </Text>
+
+      <FlatList
+        data={allSkills}
+        keyExtractor={item => item}
+        renderItem={({item}) => <SkillCard>{item}</SkillCard>}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
---
+++ myskillsapp/src/pages/Home/styles.js
@@ -0,0 +1,26 @@
+import {StyleSheet, Platform} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingTop: 70,
+    paddingBottom: 20,
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 20 : 15,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+});
