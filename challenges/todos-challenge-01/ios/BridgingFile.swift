6c82be441be4ffbf6c491881c929eba6ad616fab
ildaneta <iildaemanoely@gmail.com>
2021-08-14 18:47:45 -0300

    refactor: Removing a duplicated project

--- challenges/todos-challenge-01/ios/BridgingFile.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BridgingFile.swift
-//  todo
-//
-//  Created by Vinicius Fraga Modesto on 01/07/21.
-//
-
-import Foundation
--- challenges/todos-challenge-01/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  transformIgnorePatterns: [
-    "node_modules/(?!(@react-native|react-native|react-native-vector-icons|react-native-linear-gradient|react-native-iphone-x-helper)/)"
-  ]
-};
