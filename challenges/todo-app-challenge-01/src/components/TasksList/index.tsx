377a8410b31e6fe1672616fcebf4946641d6e322
ildaneta <iildaemanoely@gmail.com>
2021-08-16 17:18:45 -0300

    refactor: Changing the padding right value

--- challenges/todo-app-challenge-01/src/components/TasksList/index.tsx
+++ challenges/todo-app-challenge-01/src/components/TasksList/index.tsx
@@ -64,7 +64,7 @@ export function TasksList({
 
             <TouchableOpacity
               testID={`trash-${index}`}
-              style={{ paddingHorizontal: 24 }}
+              style={{ paddingRight: 20 }}
               onPress={() => removeTask(item.id)}
             >
               <Image source={trashIcon} />
