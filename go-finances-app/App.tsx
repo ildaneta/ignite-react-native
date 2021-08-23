680b8ac1474eb7e97aab50f7d70be15297de31e6
ildaneta <iildaemanoely@gmail.com>
2021-08-23 08:24:25 -0300

    feat: Installing fonts

--- go-finances-app/App.tsx
+++ go-finances-app/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StatusBar, StatusBarIOS } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
 
 import Dashboard from './src/pages/Dashboard';
 
+import theme from './src/global/styles/theme';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <SafeAreaView>
-      <Dashboard />
-    </SafeAreaView>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.background}
+      />
+
+      <ThemeProvider theme={theme}>
+        <Dashboard />
+      </ThemeProvider>
+    </>
   );
 }
