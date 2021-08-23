8a3a76d311c99da870077caa9dbc2cfbbcaa0d9e
ildaneta <iildaemanoely@gmail.com>
2021-08-23 08:26:46 -0300

    feat: Creating the dashboard page and its styles

--- go-finances-app/src/pages/Dashboard/index.tsx
+++ go-finances-app/src/pages/Dashboard/index.tsx
@@ -1,13 +1,92 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text, FlatList } from 'react-native';
 
-// import { styles } from './styles';
+import HighlightCard from '../../components/HighlightCard';
+import Header from '../../components/Header';
+import TransactionCard from '../../components/TransactionCard';
+
+import { styles } from './styles';
+
+import { ITransactionCardProps } from '../../components/TransactionCard';
+
+interface DataListProps extends ITransactionCardProps {
+  id: string;
+}
 
 const Dashboard = (): JSX.Element => {
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      title: 'Website development',
+      amount: '12.000,00',
+      category: {
+        name: 'Work',
+        icon: 'dollar-sign',
+      },
+      date: '14/04/2021',
+      type: 'positive',
+    },
+
+    {
+      id: '2',
+      title: 'iFood',
+      amount: '120,00',
+      category: {
+        name: 'Food',
+        icon: 'dollar-sign',
+      },
+      date: '14/04/2021',
+      type: 'negative',
+    },
+    {
+      id: '3',
+      title: 'Zara',
+      amount: '346,50',
+      category: {
+        name: 'Clothes',
+        icon: 'store-alt',
+      },
+      date: '14/04/2021',
+      type: 'negative',
+    },
+  ];
+
   return (
-    <View style={{ marginTop: 20 }}>
-      <Text>Dashboard</Text>
-    </View>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <Header />
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <HighlightCard
+          title="Receipts"
+          amount="17.420,00"
+          lastTransaction="April 13th"
+          type="up"
+        />
+        <HighlightCard
+          title="Outflows"
+          amount="1.259,00"
+          lastTransaction="April 3th"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="16.141,50"
+          lastTransaction="April from 1th to 16th"
+          type="total"
+        />
+      </ScrollView>
+
+      <Text style={styles.title}>Listing</Text>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ marginBottom: 40 }}
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <TransactionCard data={item} />}
+      />
+
+      <View style={{ height: 20, marginBottom: 20 }} />
+    </ScrollView>
   );
 };
 
---
+++ go-finances-app/src/pages/Dashboard/styles.ts
@@ -0,0 +1,23 @@
+
+import {Platform, StyleSheet} from 'react-native'
+import theme from '../../global/styles/theme';
+
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors.background,
+    flex: 1,
+    paddingTop: Platform.OS === 'ios' ? 66 : 26,
+    paddingHorizontal: 24,
+    paddingBottom: 30
+  },
+
+  title: {
+    fontFamily: theme.fonts.regular,
+    fontSize: 18,
+    color: theme.colors.textWhite,
+    marginBottom: 16,
+    marginTop: 32
+  }
+})
+
