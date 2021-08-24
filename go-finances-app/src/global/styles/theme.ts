5d122d0703c2e8217ca6a267d895dcded8e87ef8
ildaneta <iildaemanoely@gmail.com>
2021-08-24 07:13:26 -0300

    feat: Creating the theme styles

---
+++ go-finances-app/src/global/styles/theme.ts
@@ -0,0 +1,31 @@
+export type ThemeType = typeof theme
+
+export const theme = {
+  colors: {
+    primary: '#121015',
+    secondary: '#050505',
+    success: '#12A454',
+    success_light: 'rgba(18, 164,84, 0.5)',
+    attention: '#e83f5b',
+    attention_light: '#a32f40',
+    shape: '#f1f1f1',
+    textMediumGray: '#333',
+    textGray: '#ccc9c9',
+    textGrayDarker: '#afaeae',
+    textWhite: '#f1f1f1',
+    text: '#111',
+    background: '#1e1e21',
+    background_light: '#3a3a3a',
+    background_gray: '#777',
+    background_gray_light: '#999',
+    highLight: '#af67a3'
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold'
+  }
+}
+
+export default theme
