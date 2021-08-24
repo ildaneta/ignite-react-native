19043da066f684b105dd545f980129ca153ff82d
ildaneta <iildaemanoely@gmail.com>
2021-08-24 07:12:11 -0300

    feat: Creating the categories file

---
+++ go-finances-app/src/utils/categories.ts
@@ -0,0 +1,8 @@
+export const categories = [
+    { key: 'purchases', name: 'Purchases', icon: 'shopping-basket', color: '#5636D3' },
+    { key: 'food', name: 'Food', icon: 'coffee', color: '#FF872C' },
+    { key: 'salary', name: 'Salary', icon: 'money', color: '#12A454' },
+    { key: 'car', name: 'Car', icon: 'car', color: '#E83F5B' },
+    { key: 'leisure', name: 'Leisure', icon: 'heart-o', color: '#26195C' },
+    { key: 'studies', name: 'Studies', icon: 'book', color: '#9C001A' },
+]; 
