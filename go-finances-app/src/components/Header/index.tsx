27553a4e8be153b19ee47dae01ce32319746a062
ildaneta <iildaemanoely@gmail.com>
2021-09-12 13:50:49 -0300

    refactor: Adding borderless button from react native gesture handler

--- go-finances-app/src/components/Header/index.tsx
+++ go-finances-app/src/components/Header/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
-import theme from '../../global/styles/theme';
+import React from "react";
+import { View, Text, Image } from "react-native";
+import theme from "../../global/styles/theme";
+import {
+  BorderlessButton,
+  BorderlessButtonProperties,
+} from "react-native-gesture-handler";
 
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign } from "@expo/vector-icons";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-const Header = (): JSX.Element => {
+type LogoutButton = BorderlessButtonProperties;
+
+const Header = ({ ...rest }: LogoutButton): JSX.Element => {
   return (
     <View style={styles.containerHeader}>
       <View style={styles.containerUser}>
         <Image
           style={styles.userImage}
           source={{
-            uri: 'https://avatars.githubusercontent.com/u/21963291?v=4',
+            uri: "https://avatars.githubusercontent.com/u/21963291?v=4",
           }}
         />
 
@@ -23,9 +29,9 @@ const Header = (): JSX.Element => {
         </View>
       </View>
 
-      <TouchableWithoutFeedback style={styles.logoff}>
+      <BorderlessButton style={styles.logoff} {...rest}>
         <AntDesign name="poweroff" color={theme.colors.attention} size={24} />
-      </TouchableWithoutFeedback>
+      </BorderlessButton>
     </View>
   );
 };
