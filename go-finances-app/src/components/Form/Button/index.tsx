317e73cb3a4bfec03f1c861c97a4944719331675
ildaneta <iildaemanoely@gmail.com>
2021-09-12 13:50:12 -0300

    refactor: Adding new styles

--- go-finances-app/src/components/Form/Button/index.tsx
+++ go-finances-app/src/components/Form/Button/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { TouchableOpacity, TouchableOpacityProps, Text } from 'react-native';
+import React from "react";
+import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 interface ButtonProps extends TouchableOpacityProps {
   label: string;
 }
 
 const Button = ({ label, ...rest }: ButtonProps): JSX.Element => {
+  const { style } = { ...rest };
+
   return (
-    <TouchableOpacity {...rest} style={styles.container} activeOpacity={0.8}>
+    <TouchableOpacity
+      {...rest}
+      style={[styles.container, style]}
+      activeOpacity={0.8}
+    >
       <Text style={styles.buttonText}>{label}</Text>
     </TouchableOpacity>
   );
