ab9c94ad0d848472d8d2f89fa4b9b4c6548005f8
ildaneta <iildaemanoely@gmail.com>
2021-09-12 13:49:45 -0300

    feat: Adding the navigation container and routes

--- go-finances-app/App.tsx
+++ go-finances-app/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { LogBox, StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components';
+import "react-native-gesture-handler";
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
+
+import React from "react";
+import { LogBox, SafeAreaView, StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold,
-} from '@expo-google-fonts/poppins';
-import AppLoading from 'expo-app-loading';
+} from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
 
-import Dashboard from './src/pages/Dashboard';
-import Register from './src/pages/Register';
-import CategorySelect from './src/pages/CategorySelect';
+import { NavigationContainer } from "@react-navigation/native";
 
-import theme from './src/global/styles/theme';
+import AppRoutes from "./src/routes/app.routes";
+
+import theme from "./src/global/styles/theme";
 
 LogBox.ignoreLogs([
-  'VirtualizedLists should never be nested inside plain ScrollViews with the same orientation - use another VirtualizedList-backed container instead. at node_modules/react-native/Libraries/Lists/VirtualizedList.js:1195:14 in ScrollView.Context.Consumer.props.children at node_modules/@expo-google-fonts/poppins/useFonts.js:21:27 in loadAsync.then$argument_0'
-])
+  "VirtualizedLists should never be nested inside plain ScrollViews with the same orientation - use another VirtualizedList-backed container instead. at node_modules/react-native/Libraries/Lists/VirtualizedList.js:1195:14 in ScrollView.Context.Consumer.props.children at node_modules/@expo-google-fonts/poppins/useFonts.js:21:27 in loadAsync.then$argument_0",
+]);
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -33,11 +37,20 @@ export default function App() {
     <>
       <StatusBar
         barStyle="light-content"
-        backgroundColor={theme.colors.background}
+        backgroundColor={theme.colors.secondary}
       />
 
       <ThemeProvider theme={theme}>
-        <Register />
+        <SafeAreaView
+          style={{
+            flex: 1,
+            backgroundColor: theme.colors.secondary,
+          }}
+        >
+          <NavigationContainer>
+            <AppRoutes />
+          </NavigationContainer>
+        </SafeAreaView>
       </ThemeProvider>
     </>
   );
