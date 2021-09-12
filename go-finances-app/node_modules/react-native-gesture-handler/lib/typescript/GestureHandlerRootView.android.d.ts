4339c47aad3612797600f5bed4d51e1a255b29df
ildaneta <iildaemanoely@gmail.com>
2021-09-12 13:30:07 -0300

    chore: Removing node_modules

--- go-finances-app/node_modules/react-native-gesture-handler/lib/typescript/GestureHandlerRootView.android.d.ts
+++
@@ -1,4 +0,0 @@
-import { PropsWithChildren } from 'react';
-declare type Props = PropsWithChildren<Record<string, unknown>>;
-export default function GestureHandlerRootView({ children, ...rest }: Props): JSX.Element;
-export {};
--- go-finances-app/src/pages/Register/index.tsx
+++ go-finances-app/src/pages/Register/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/core";
 import {
   View,
   StatusBar,
@@ -7,19 +8,21 @@ import {
   Keyboard,
   TouchableWithoutFeedback,
   Alert,
-} from 'react-native';
-import { useForm } from 'react-hook-form';
-import * as Yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
+} from "react-native";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import uuid from "uuid";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-import InputForm from '../../components/Form/InputForm';
-import Button from '../../components/Form/Button';
-import TypeButton from '../../components/Form/TypeButton';
-import InputSelect from '../../components/Form/InputSelect';
-import CategorySelect from '../CategorySelect';
-import theme from '../../global/styles/theme';
+import InputForm from "../../components/Form/InputForm";
+import Button from "../../components/Form/Button";
+import TypeButton from "../../components/Form/TypeButton";
+import InputSelect from "../../components/Form/InputSelect";
+import CategorySelect from "../CategorySelect";
+import theme from "../../global/styles/theme";
 
 interface IFormData {
   name: string;
@@ -27,25 +30,28 @@ interface IFormData {
 }
 
 const schema = Yup.object().shape({
-  name: Yup.string().required('Name is required'),
+  name: Yup.string().required("Name is required"),
   price: Yup.number()
-    .typeError('Please, insert a numeric value')
-    .required('Price is required')
+    .typeError("Please, insert a numeric value")
+    .required("Price is required")
     .positive(`The value can't be negative'`),
 });
 
 const Register = (): JSX.Element => {
+  const navigation = useNavigation();
+
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
-  const [transactionType, setTransactionType] = useState('');
+  const [transactionType, setTransactionType] = useState("");
 
   const [category, setCategory] = useState({
-    key: 'category',
-    name: 'Categoria',
+    key: "category",
+    name: "Categoria",
   });
 
   const {
     control,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
 
@@ -57,92 +63,114 @@ const Register = (): JSX.Element => {
     setCategoryModalOpen(false);
   };
 
-  const handleTransactionType = (type: 'Income' | 'Outcome') => {
+  const handleTransactionType = (type: "Income" | "Outcome") => {
     setTransactionType(type);
   };
 
-  const handleSendRegister = (form: IFormData) => {
-    if (!transactionType) return Alert.alert('Selecione o tipo da transação');
+  const handleSendRegister = async (form: IFormData) => {
+    if (!transactionType) return Alert.alert("Selecione o tipo da transação");
 
-    if (category.key === 'category')
-      return Alert.alert('Selecione a categoria');
+    if (category.key === "category")
+      return Alert.alert("Selecione a categoria");
 
-    const data = {
+    const newTransactionData = {
+      id: String(uuid.v4()),
       name: form.name,
-      price: form.price,
+      amount: form.price,
       category,
-      transactionType,
+      type: transactionType,
+      date: new Date(),
     };
 
-    return console.log(data);
+    try {
+      const transactionsKey = "@gofinances:transactions";
+      const data = await AsyncStorage.getItem(transactionsKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [...currentData, newTransactionData];
+
+      await AsyncStorage.setItem(
+        transactionsKey,
+        JSON.stringify(dataFormatted)
+      );
+
+      reset();
+      setTransactionType("");
+      setCategory({ key: "category", name: "Categoria" });
+      navigation.navigate("Listing");
+    } catch (error) {
+      console.log(error.response);
+      Alert.alert("Não foi possível salver");
+    }
   };
 
   return (
-    <>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor={theme.colors.secondary}
-      />
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <SafeAreaView style={styles.container}>
-          <View style={styles.form}>
-            <InputForm
-              control={control}
-              nameInput="name"
-              placeholder="Name"
-              autoCapitalize="sentences"
-              autoCorrect={false}
-              isError={errors.name ? true : false}
-              errorMessage={errors.name && errors.name.message}
-            />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.form}>
+          <InputForm
+            control={control}
+            nameInput="name"
+            placeholder="Name"
+            autoCapitalize="sentences"
+            autoCorrect={false}
+            isError={errors.name ? true : false}
+            errorMessage={errors.name && errors.name.message}
+          />
 
-            <View style={styles.divider} />
+          <View style={styles.divider} />
 
-            <InputForm
-              control={control}
-              nameInput="price"
-              placeholder="Price"
-              keyboardType="numeric"
-              isError={errors.price ? true : false}
-              errorMessage={errors.price && errors.price.message}
-            />
-          </View>
+          <InputForm
+            control={control}
+            nameInput="price"
+            placeholder="Price"
+            keyboardType="numeric"
+            isError={errors.price ? true : false}
+            errorMessage={errors.price && errors.price.message}
+          />
+        </View>
 
-          <View style={styles.containerTypeButton}>
-            <TypeButton
-              label="Income"
-              onPress={() => handleTransactionType('Income')}
-              type="Income"
-              isActive={transactionType === 'Income'}
-            />
-            <TypeButton
-              label="Outcome"
-              onPress={() => handleTransactionType('Outcome')}
-              type="Outcome"
-              isActive={transactionType === 'Outcome'}
-            />
-          </View>
+        <View style={styles.containerTypeButton}>
+          <TypeButton
+            label="Income"
+            onPress={() => handleTransactionType("Income")}
+            type="Income"
+            isActive={transactionType === "Income"}
+          />
+          <TypeButton
+            label="Outcome"
+            onPress={() => handleTransactionType("Outcome")}
+            type="Outcome"
+            isActive={transactionType === "Outcome"}
+          />
+        </View>
 
-          <View style={styles.containerInputSelect}>
-            <InputSelect
-              categoryTitle={category.name}
-              onPress={handleOpenCategoryModal}
-            />
-          </View>
-          <View style={styles.containerButton}>
-            <Button label="Send" onPress={handleSubmit(handleSendRegister)} />
-          </View>
+        <View style={styles.containerInputSelect}>
+          <InputSelect
+            categoryTitle={category.name}
+            onPress={handleOpenCategoryModal}
+          />
+        </View>
 
-          <Modal visible={categoryModalOpen} animationType="slide">
-            <CategorySelect
-              category={category}
-              setCategory={setCategory}
-              closeSelectCategory={handleCloseCategoryModal}
-            />
-          </Modal>
-        </SafeAreaView>
-      </TouchableWithoutFeedback>
-    </>
+        <View style={styles.containerButton}>
+          <Button
+            label="Send"
+            onPress={handleSubmit(handleSendRegister)}
+            style={{
+              backgroundColor: theme.colors.secondary,
+            }}
+          />
+        </View>
+
+        <Modal visible={categoryModalOpen} animationType="slide">
+          <CategorySelect
+            category={category}
+            setCategory={setCategory}
+            closeSelectCategory={handleCloseCategoryModal}
+          />
+        </Modal>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
