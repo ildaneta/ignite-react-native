03c1dd72939bca76f03a6ba5d72b88550ed9ab44
ildaneta <iildaemanoely@gmail.com>
2021-09-12 13:51:30 -0300

    refactor: Adding new styles and changing the comma

--- go-finances-app/src/pages/CategorySelect/index.tsx
+++ go-finances-app/src/pages/CategorySelect/index.tsx
@@ -1,49 +1,59 @@
-import React from 'react';
-import { FlatList, SafeAreaView, View } from 'react-native';
+import React from "react";
+import { FlatList, SafeAreaView, View, Text, StatusBar } from "react-native";
 
-import Category from '../../components/Category';
-import Button from '../../components/Form/Button';
-import { categories } from '../../utils/categories';
+import Category from "../../components/Category";
+import Button from "../../components/Form/Button";
+import theme from "../../global/styles/theme";
+import { categories } from "../../utils/categories";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 interface Category {
-  key: string
-  name: string
+  key: string;
+  name: string;
 }
 
 interface ICategorySelectProps {
   category: Category;
-  setCategory: (category: Category) => void
-  closeSelectCategory: () => void
+  setCategory: (category: Category) => void;
+  closeSelectCategory: () => void;
 }
 
 const CategorySelect = ({
-  category, 
-  setCategory, 
-  closeSelectCategory
+  category,
+  setCategory,
+  closeSelectCategory,
 }: ICategorySelectProps): JSX.Element => {
   const handleCategorySelect = (item: Category) => {
-    setCategory(item)
-  }
+    setCategory(item);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
+      <Text style={styles.headerText}>Select a category</Text>
+
       <FlatList
         data={categories}
         keyExtractor={(item) => item.key}
         renderItem={({ item }) => (
-          <Category 
-            categoryName={item.name} 
-            iconName={item.icon} 
+          <Category
+            categoryName={item.name}
+            iconName={item.icon}
             onPress={() => handleCategorySelect(item)}
             isActive={category.key === item.key}
+            activeOpacity={0.8}
           />
         )}
       />
 
       <View style={styles.containerButton}>
-        <Button label="Select" onPress={closeSelectCategory}/>
+        <Button
+          label="Select"
+          onPress={closeSelectCategory}
+          style={{
+            backgroundColor: theme.colors.secondary,
+          }}
+        />
       </View>
     </SafeAreaView>
   );
