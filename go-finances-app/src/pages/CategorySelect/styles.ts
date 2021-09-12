ce2d498aebeb27df40b743c988961537df64107d
ildaneta <iildaemanoely@gmail.com>
2021-09-12 13:51:54 -0300

    refactor: Adding a new style for headerText and changing the background color

--- go-finances-app/src/pages/CategorySelect/styles.ts
+++ go-finances-app/src/pages/CategorySelect/styles.ts
@@ -4,11 +4,19 @@ import theme from '../../global/styles/theme';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: theme.colors.background
+    backgroundColor: theme.colors.background,
   },
 
   containerButton: {
     marginBottom: Platform.OS === 'android' ? 20 : 0,
     marginHorizontal: 12
+  },
+
+  headerText: {
+    color: theme.colors.shape,
+    fontSize: 16,
+    fontFamily: theme.fonts.medium,
+    alignSelf: 'center',
+    marginVertical: 20,
   }
 })
