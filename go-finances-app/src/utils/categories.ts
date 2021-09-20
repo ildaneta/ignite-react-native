cc988686024062b90057e9d036dea0d3172524c9
ildaneta <iildaemanoely@gmail.com>
2021-09-20 05:00:49 -0300

    refactor: Changing the category colors

--- go-finances-app/src/utils/categories.ts
+++ go-finances-app/src/utils/categories.ts
@@ -1,8 +1,8 @@
 export const categories = [
-    { key: 'purchases', name: 'Purchases', icon: 'shopping-basket', color: '#ff51fc' },
-    { key: 'food', name: 'Food', icon: 'coffee', color: '#FF872C' },
-    { key: 'salary', name: 'Salary', icon: 'money', color: '#4ae04c' },
-    { key: 'car', name: 'Car', icon: 'car', color: '#f26' },
-    { key: 'leisure', name: 'Leisure', icon: 'heart-o', color: '#6af7f5' },
-    { key: 'studies', name: 'Studies', icon: 'book', color: '#263bff' },
+    { key: 'purchases', name: 'Purchases', icon: 'shopping-basket', color: '#F6D2D6' },
+    { key: 'food', name: 'Food', icon: 'coffee', color: '#D46A92' },
+    { key: 'salary', name: 'Salary', icon: 'money', color: '#856873' },
+    { key: 'car', name: 'Car', icon: 'car', color: '#E79A92' },
+    { key: 'leisure', name: 'Leisure', icon: 'heart-o', color: '#F97171' },
+    { key: 'studies', name: 'Studies', icon: 'book', color: '#765A74' },
 ]; 
