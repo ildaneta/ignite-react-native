0d60aed4a3c018c4557d0f7a249e28f3fad5a198
ildaneta <iildaemanoely@gmail.com>
2021-09-23 18:39:24 -0300

    feat: Creating the LogIn page

---
+++ go-finances-app/src/pages/LogIn/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { View, Text, Platform, Alert } from "react-native";
+
+import { useTheme } from "styled-components";
+
+import AppleIcon from "../../assets/apple-icon.svg";
+import GoogleIcon from "../../assets/google-icon.svg";
+import GoFinancesIcon from "../../assets/finance-icon.svg";
+
+import ButtonIcon from "../../components/ButtonIcon";
+
+import { styles } from "./styles";
+import { useAuthContext } from "../../context/AuthContext";
+
+const LogIn = (): JSX.Element => {
+  const { signInWithGoogle } = useAuthContext();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const theme = useTheme();
+
+  async function handleSignInWithGoogle() {
+    try {
+      setIsLoading(true);
+      return await signInWithGoogle();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível conectar a conta Google");
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <GoFinancesIcon />
+      <Text style={styles.title}>GoFinances</Text>
+
+      <Text numberOfLines={3} style={styles.description}>
+        A very simple control for your Personal Finances
+      </Text>
+
+      <Text numberOfLines={2} style={styles.textLogin}>
+        Login with one of the accounts below
+      </Text>
+
+      <ButtonIcon
+        label="LogIn with Google"
+        icon={<GoogleIcon />}
+        onPress={handleSignInWithGoogle}
+      />
+
+      {Platform.OS === "ios" && (
+        <ButtonIcon label="LogIn with Apple" icon={<AppleIcon />} />
+      )}
+    </View>
+  );
+};
+
+export default LogIn;
---
+++ go-finances-app/src/pages/LogIn/styles.ts
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+import theme from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    alignItems: 'center',
+    paddingTop: 60,
+    backgroundColor:  theme.colors.background
+  },
+
+  title: {
+    marginTop: 10,
+    marginBottom: 30,
+    fontFamily: theme.fonts.medium,
+    fontSize: 18,
+    color: theme.colors.highLight_menu,
+
+  },
+
+  description: {
+    width: 290,
+    fontFamily: theme.fonts.medium,
+    fontSize: 30,
+    textAlign: 'center',
+    color: theme.colors.shape,
+    marginBottom: 60
+  },
+
+  textLogin: {
+    width: 169,
+    fontFamily: theme.fonts.regular,
+    fontSize: 16,
+    textAlign: 'center',
+    color: theme.colors.shape,
+    marginBottom: 40
+  }
+
+}) 
