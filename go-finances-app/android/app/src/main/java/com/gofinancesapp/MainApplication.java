78d628c7174cf117d097ca7af13208cb404da699
ildaneta <iildaemanoely@gmail.com>
2021-10-16 21:00:18 -0300

    config: Changing the application name

--- go-finances-app/android/app/src/main/java/com/gofinancesapp/MainApplication.java
+++ go-finances-app/android/app/src/main/java/com/gofinancesapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.gofinancesapp;
+package com.gofinances;
 
 import android.app.Application;
 import android.content.Context;
@@ -11,7 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.gofinancesapp.generated.BasePackageList;
+import com.gofinances.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
@@ -110,7 +110,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.gofinancesapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.gofinances.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
