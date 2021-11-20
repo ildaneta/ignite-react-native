7f574e6de4a19bb98627e5436862fdac84462ebf
ildaneta <iildaemanoely@gmail.com>
2021-11-20 07:26:15 -0300

    refactor: removing inline styles

--- challenges/todo-app-challenge-01/src/components/TasksList/index.tsx
+++ challenges/todo-app-challenge-01/src/components/TasksList/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { FlatList } from 'react-native';
-import { IEditTaskProps } from '../../pages/Home';
+import { IEditTaskProps } from '../../pages/Home/Home';
+import { styles } from './styles';
 
 import { ItemWrapper } from '../ItemWrapper/index';
-import TaskItem, { ITask } from '../TaskItem';
+import TaskItem from '../TaskItem';
+
+export interface ITask {
+  id: number;
+  title: string;
+  done: boolean;
+}
 
 interface TasksListProps {
   tasks: ITask[];
@@ -22,7 +29,7 @@ export function TasksList({
     <FlatList
       data={tasks}
       keyExtractor={(item) => String(item.id)}
-      contentContainerStyle={{ paddingBottom: 24 }}
+      contentContainerStyle={styles.contentContainer}
       showsVerticalScrollIndicator={false}
       renderItem={({ item, index }) => {
         return (
@@ -37,9 +44,7 @@ export function TasksList({
           </ItemWrapper>
         );
       }}
-      style={{
-        marginTop: 32,
-      }}
+      style={styles.flatList}
     />
   );
 }
