baf973c41e1790a66fbf748c888189343e5702ee
ildaneta <iildaemanoely@gmail.com>
2021-11-20 07:26:29 -0300

    feat:  creating the tasks list styles

---
+++ challenges/todo-app-challenge-01/src/components/TasksList/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  contentContainer: {
+    paddingBottom: 24
+  },
+
+  flatList: {
+    marginTop: 32
+  }
+})
