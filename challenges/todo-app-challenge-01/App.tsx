e789af59c03d587c15f128865fc5ba5181b0c4b2
ildaneta <iildaemanoely@gmail.com>
2021-11-20 07:24:37 -0300

    refactor: Changing the home exportation

--- challenges/todo-app-challenge-01/App.tsx
+++ challenges/todo-app-challenge-01/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
-import { Home } from './src/pages/Home';
+import { Home } from './src/pages/Home/Home';
 
 export default function App() {
   return (
     <>
-      <StatusBar 
-        barStyle="light-content" 
-        translucent 
-        backgroundColor="transparent" 
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
       />
       <Home />
     </>
