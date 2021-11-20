5e931ab7dcb925ac0c809ada977d309ebab7752f
ildaneta <iildaemanoely@gmail.com>
2021-11-20 07:25:18 -0300

    feat: creating the item wrapper styles

---
+++ challenges/todo-app-challenge-01/src/components/ItemWrapper/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+})
