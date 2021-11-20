fdbd09bf7464b00ba62cd96b62d7d174b4452197
ildaneta <iildaemanoely@gmail.com>
2021-11-20 07:24:59 -0300

    refactor: removing inline styles

--- challenges/todo-app-challenge-01/src/components/ItemWrapper/index.tsx
+++ challenges/todo-app-challenge-01/src/components/ItemWrapper/index.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode } from 'react';
 import { View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
+import { styles } from './styles';
+
 interface ItemWrapperProps {
   index: number;
   children: ReactNode;
@@ -14,12 +16,7 @@ export function ItemWrapper({ index, children }: ItemWrapperProps) {
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
         colors={['rgba(196, 196, 196, 0)', 'rgba(196, 196, 196, 0.24)']}
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}
+        style={styles.container}
       >
         {children}
       </LinearGradient>
