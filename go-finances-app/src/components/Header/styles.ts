fa5c9f0d436c22723133a13df829c8ad2cfca76c
ildaneta <iildaemanoely@gmail.com>
2021-12-16 11:11:20 -0300

    refactor: adding a marginTop

--- go-finances-app/src/components/Header/styles.ts
+++ go-finances-app/src/components/Header/styles.ts
@@ -6,7 +6,8 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginBottom: 22,
-    alignItems:'center'
+    alignItems:'center',
+    marginTop: 20
   },
 
   containerUser: {
