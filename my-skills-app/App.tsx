c1f2941e9d7470b4ac201e098d0f6dba6d9720aa
ildaneta <iildaemanoely@gmail.com>
2021-12-21 19:38:44 -0300

    feat: Adding a configuration to splash screen

--- my-skills-app/App.tsx
+++ my-skills-app/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import Home from './src/pages/Home/index';
 
 const App = (): JSX.Element => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <>
       <StatusBar backgroundColor="#121015" barStyle={'light-content'} />
